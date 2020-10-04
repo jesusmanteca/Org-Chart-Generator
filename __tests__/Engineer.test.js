328a369d6731d70dd8c86247760b3154878e121e
Jesus Manteca <jesusmanteca@gmail.com>
2020-10-04 17:01:16 -0500

    finished testing intern and engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require("../lib/Engineer");
+
+test('get gitHub with constructor', () => {
+    const gitHubUserName = "Chicano123"
+    const engineer = new Engineer("Arturo", 123, "Art@email.com", gitHubUserName)
+    expect(engineer.gitHub).toBe(gitHubUserName)
+})
+
+test('get gitHub username with getGitHub()', ()=>{
+    const username = "Chicano123"
+    const dude = new Engineer("Arturo", 123, "Art@email.com", username)
+    expect(dude.getGitHub()).toBe(username)
+})
+
+test('get "Engineer" with getRole()', ()=>{
+    const occupation = "Engineer"
+    const miss = new Engineer("Laura", 321, "Laura@email.com", "Antrope@github.com")
+    expect(miss.getRole()).toBe(occupation)
+})
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
 const Intern = require("../lib/Intern")
+
+test('get school through constuctor', ()=>{
+    const education = "School of Hard Knocks"
+    const NKOTB = new Intern('Mel', 123, 'mel@email.com', education)
+    expect(NKOTB.school).toBe(education)
+})
+
+test('getSchool() should get school', ()=>{
+    const school = "School of Hard Knocks"
+    const NKOTB = new Intern('Melvin', 123, 'mel@email.com', school)
+    expect(NKOTB.getSchool()).toBe(school)
+})
+
+test('getRole() should return "Intern"', ()=>{
+    const position = "Intern"
+    const NKOTB = new Intern('Melissa', 123, 'mel@email.com', "School of Hard Knocks")
+    expect(NKOTB.getRole()).toBe(position)
+})
